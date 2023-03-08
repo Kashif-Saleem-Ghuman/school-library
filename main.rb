7b5666a461452269a005a15366931c3a7b38a22f
unknown <kashif_saleem_23@hotmail.com>
2023-03-08 12:53:12 +0500

    correct linter

--- main.rb
+++ main.rb
@@ -6,5 +6,5 @@ person = Person.new(22, 'maximilianus')
 p person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 p capitalized_person.correct_name
-capitalized_trimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-p capitalized_trimmedPerson.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+p capitalized_trimmed_person.correct_name
