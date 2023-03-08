ee655ae482bb796b1a887275786f8a97c4ad936f
unknown <kashif_saleem_23@hotmail.com>
2023-03-08 12:47:54 +0500

    add main.rb file

---
+++ main.rb
@@ -0,0 +1,10 @@
+require_relative 'Person'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+person = Person.new(22, 'maximilianus')
+p person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+p capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+p capitalizedTrimmedPerson.correct_name
--- person.rb
+++ person.rb
@@ -1,6 +1,4 @@
 require_relative 'nameable'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
 
 class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
