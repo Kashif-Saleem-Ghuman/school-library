02737f7c8016d7926ded5008b797bdb7ecb23543
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-03-13 20:15:45 +0530

    refactor main and created seperate class

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@ require_relative 'app'
 def main
   app = App.new
   app.start_console
+ 
 end
 
 def list_of_options
@@ -14,25 +15,6 @@ def list_of_options
   puts '5 - Create a rental'
   puts '6 - List all rentals for a given person id'
   puts '7 - Exit'
-end
-
-def option(input)
-  case input
-  when '1'
-    list_all_books
-  when '2'
-    list_all_persons
-  when '3'
-    create_person
-  when '4'
-    create_book
-  when '5'
-    create_rental
-  when '6'
-    list_rentals
-  else
-    puts 'Invalid option'
-  end
 end
 
 main
